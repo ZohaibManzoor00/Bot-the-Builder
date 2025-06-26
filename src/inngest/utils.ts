5ac13e173dfe70de2d19689ac419d25de6ed2d4d
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-06-26 15:58:48 -0400

    code generation working with live next.js link

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,21 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export const getSandbox = async (sandboxId: string) => {
   const sandbox = await Sandbox.connect(sandboxId);
   return sandbox;
+};
+
+export const lastAssistantTextMessageContent = async (result: AgentResult) => {
+  const lastAssistantTextMessageIdx = result.output.findLastIndex(
+    (message) => message.role == "assistant"
+  );
+  const message = result.output[lastAssistantTextMessageIdx] as
+    | TextMessage
+    | undefined;
+  return message?.content
+    ? typeof message.content === "string"
+      ? message.content
+      : message.content.map((c) => c.text).join("")
+    : undefined;
 };
