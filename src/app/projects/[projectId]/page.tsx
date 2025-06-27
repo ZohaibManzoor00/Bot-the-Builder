6a01c3aa094110e851a1f4f2644f8a93fa912dbe
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-06-26 20:40:52 -0400

    completed messages ui

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,9 +1,27 @@
+import { ProjectView } from "@/modules/projects/server/ui/views/project-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
   params: Promise<{ projectId: string }>;
 }
 
 export default async function ProjectPage({ params }: Props) {
   const { projectId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({ projectId })
+  );
+  void queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({ id: projectId })
+  );
 
-  return <div>Project ID: {projectId}</div>;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
+  );
 }
---
+++ src/modules/projects/server/ui/views/components/messages-container.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+
+import { MessageCard } from "./message-card";
+import { MessagesForm } from "./messages-form";
+
+interface Props {
+  projectId: string;
+}
+
+export function MessagesContainer({ projectId }: Props) {
+  const trpc = useTRPC();
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const { data: messages } = useSuspenseQuery(
+    trpc.messages.getMany.queryOptions({ projectId })
+  );
+
+  useEffect(() => {
+    const lastAssistanceMessage = messages.findLast(
+      (message) => message.role === "ASSISTANT"
+    );
+    if (lastAssistanceMessage) {
+      //   bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className="flex flex-col flex-1 min-h-0">
+      <div className="flex-1 min-h-0 overflow-y-auto">
+        <div className="pt-2 pr-1">
+          {messages.map((message) => (
+            <MessageCard
+              key={message.id}
+              content={message.content}
+              fragment={message.fragment}
+              role={message.role}
+              createdAt={message.createdAt}
+              isActiveFragment={false}
+              onFragmentClick={() => {}}
+              type={message.type}
+            />
+          ))}
+        </div>
+        <div ref={bottomRef} />
+      </div>
+      <div className="relative p-3 pt-1">
+        <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" />
+
+        <MessagesForm projectId={projectId} />
+      </div>
+    </div>
+  );
+}
