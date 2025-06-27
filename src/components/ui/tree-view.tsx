9d7825598c1cf4561351c8732e0007e0e1ccfacd
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-06-27 12:58:20 -0400

    tabs in project view, file tree, file path breadcrumbs

---
+++ src/components/ui/tree-view.tsx
@@ -0,0 +1,113 @@
+import { ChevronRightIcon, FileIcon, FolderIcon } from "lucide-react";
+import { type TreeItem } from "@/lib/utils";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+
+interface TreeViewProps {
+  data: TreeItem[];
+  value?: string | null;
+  onSelect?: (value: string) => void;
+}
+
+export function TreeView({ data, value, onSelect }: TreeViewProps) {
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="none" className="w-full">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {data.map((item, idx) => (
+                  <Tree
+                    key={idx}
+                    item={item}
+                    selectedValue={value}
+                    onSelect={onSelect}
+                    parentPath=""
+                  />
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarRail />
+      </Sidebar>
+    </SidebarProvider>
+  );
+}
+
+interface TreeProps {
+  item: TreeItem;
+  selectedValue?: string | null;
+  onSelect?: (value: string) => void;
+  parentPath: string;
+}
+
+const Tree = ({ item, selectedValue, onSelect, parentPath }: TreeProps) => {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+  const currentPath = parentPath ? `${parentPath}/${name}` : name;
+
+  const isFile = !items.length;
+  if (isFile) {
+    const isSelected = selectedValue === currentPath;
+
+    return (
+      <SidebarMenuButton
+        isActive={isSelected}
+        className="data-[active=true]:bg-transparent"
+        onClick={() => onSelect?.(currentPath)}
+      >
+        <FileIcon />
+        <span className="truncate">{name}</span>
+      </SidebarMenuButton>
+    );
+  }
+
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRightIcon className="transition-transform" />
+            <FolderIcon />
+            <span className="truncate">{name}</span>
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {items.map((subItem, idx) => (
+              <Tree
+                key={idx}
+                item={subItem}
+                selectedValue={selectedValue}
+                onSelect={onSelect}
+                parentPath={currentPath}
+              />
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
+  );
+};
--- src/modules/projects/server/ui/views/project-view.tsx
+++ src/modules/projects/server/ui/views/project-view.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Suspense, useState } from "react";
+import Link from "next/link";
 import { Fragment } from "@/generated/prisma";
 
 import {
@@ -8,9 +9,13 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { MessagesContainer } from "./components/messages-container";
 import { ProjectHeader } from "./components/project-header";
 import { FragmentWeb } from "./components/fragment-web";
+import { CodeIcon, CrownIcon, EyeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { FileExplorer } from "@/components/ui/file-explorer";
 
 interface Props {
   projectId: string;
@@ -18,6 +23,7 @@ interface Props {
 
 export function ProjectView({ projectId }: Props) {
   const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabState, setTabState] = useState<"preview" | "code">("preview");
 
   return (
     <div className="h-screen">
@@ -42,7 +48,41 @@ export function ProjectView({ projectId }: Props) {
         <ResizableHandle withHandle />
 
         <ResizablePanel defaultSize={65} minSize={50}>
-          {!!activeFragment && <FragmentWeb data={activeFragment} />}
+          <Tabs
+            className="h-full gap-y-0"
+            defaultValue="preview"
+            value={tabState}
+            onValueChange={(value) => setTabState(value as "preview" | "code")}
+          >
+            <div className="w-full flex items-center p-2 border-b gap-x-2">
+              <TabsList className="h-8 p-0 border rounded-md">
+                <TabsTrigger value="preview" className="rounded-md">
+                  <EyeIcon /> <span>Demo</span>
+                </TabsTrigger>
+                <TabsTrigger value="code" className="rounded-md">
+                  <CodeIcon /> <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+              <div className="ml-auto flex items-center gap-x-2">
+                <Button asChild size="sm">
+                  <Link href="/pricing">
+                    <CrownIcon /> Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <TabsContent value="preview">
+              {!!activeFragment && <FragmentWeb data={activeFragment} />}
+            </TabsContent>
+
+            <TabsContent value="code" className="min-h-0">
+              <div className="h-full w-full">
+                {!!activeFragment?.files && (
+                  <FileExplorer files={activeFragment.files as { [path: string]: string }} />
+                )}
+              </div>
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
